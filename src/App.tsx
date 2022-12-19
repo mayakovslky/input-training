78ba932f562503cfe4cd1316c5df4ac2d92edb4e
mayakovsky <mayakovsky03@gmail.com>
2022-12-19 19:05:30 +0300

    06. input training part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import {FullInput} from "./components/FullInput";
 
 function App() {
+    let [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
+            {message: 'message4'},
+            {message: 'message5'}
+        ]
+    )
+
+    const addMessage = (title: string) => {
+        let newMessage = {message: title};
+
+        setMessage([newMessage,...message])
+    }
+
     return (
-
-        <>
-            <div>sdf</div>
-            <div>sdf</div>
-        </>
-
+        <div className="App">
+            {/*<div>*/}
+            {/*    <input />*/}
+            {/*    <button>+</button>*/}
+            {/*</div>*/}
+            <FullInput addMessage={addMessage}/>
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
     );
-
 }
 
 export default App;
